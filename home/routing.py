cd975a760be0b756de832801e9e135c44a9ff5b4
aslammiya <aslammiya5257@gmail.com>
2024-08-09 00:03:54 +0530

    channels added

--- home/routing.py
+++ home/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/ac/', consumers.MyAsyncWebsocketConsumer.as_asgi()),
+    path('ws/ac', consumers.MyAsyncWebsocketConsumer.as_asgi()),
 ]
