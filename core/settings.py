84ba55940883bf2856d49ffea2990b3be6658b73
aslammiya <aslammiya5257@gmail.com>
2024-08-17 18:36:12 +0530

    render deploy

--- core/settings.py
+++ core/settings.py
@@ -16,7 +16,7 @@ DEBUG = False
 
 # ALLOWED_HOSTS = ['127.0.0.1', '*']
 
-ALLOWED_HOSTS = ['.render.com', 'echospace.onrender.com']
+ALLOWED_HOSTS = ['.render.com', 'echospace.onrender.com', '.vercel.app']
 CSRF_TRUSTED_ORIGINS = ['https://echospace.onrender.com']
 
 # Application definition
